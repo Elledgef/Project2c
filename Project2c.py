9d4116f8f69a66e64bed1eb90306831279e7e91d
elledgef <Elledgef@oregonstate.edu>
2022-04-06 23:36:05 -0700

    Initial commit

---
+++ Project2c.py
@@ -0,0 +1,12 @@
+change = int(input("Please enter an amount in cents less than a dollar."))
+print(change)
+print("Your change will be:")
+print("Q:", change//25)
+change = change%25
+print("D:", change//10)
+change = change%10
+print("N:",change//5)
+change = change%5
+print("P:",change//1)
+change - change%1
+
